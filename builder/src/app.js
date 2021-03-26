cc199654633b78dd6976f1f8d64016d95f58edd7
younggil <zelord.kwoun@gmail.com>
2021-03-26 11:06:19 +0900

    [ADD] STEP3 PR (#38)

--- builder/src/app.js
+++ builder/src/app.js
@@ -1,11 +1,24 @@
-import { renderProduct } from "./productBox";
+import { WalletModel } from "./js/model/WalletModel";
+import { ProductModel } from "./js/model/ProductModel";
+
 import { WalletView } from "./js/view/WalletView";
-import { WalletModel} from "./js/model/WalletModel";
+import { ScreenView } from "./js/view/ScreenView";
+import { ProductView } from "./js/view/ProductView"
 
-renderProduct();
+const walletModel = new WalletModel;
+const productModel = new ProductModel;
 
-const model = new WalletModel;
-const view = new WalletView(model);
-view.update();
+const walletView = new WalletView(walletModel);
+const screenView = new ScreenView(walletModel);
+const productView = new ProductView(walletModel, productModel);
 
-model.addObserver(view);
+walletView.init();
+screenView.init();
+productView.init();
+
+walletModel.addObserver(walletView);
+walletModel.addObserver(screenView);
+walletModel.addObserver(productView);
+productModel.addObserver(productView);
+
+
